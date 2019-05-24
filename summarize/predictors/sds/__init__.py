2bd0521b2d527c61445a964a06ac05c3458f2de8
Daniel Deutsch <danfdeutsch@gmail.com>
2019-05-24 18:58:51 -0400

    Adding an abstractive predictor

---
+++ summarize/predictors/sds/__init__.py
@@ -0,0 +1,1 @@
+from summarize.predictors.sds.abstractive import AbstractivePredictor
---
+++ summarize/predictors/sds/abstractive.py
@@ -0,0 +1,18 @@
+import json
+from allennlp.common.util import JsonDict
+from allennlp.data import Instance
+from allennlp.service.predictors.predictor import Predictor
+from overrides import overrides
+
+
+@Predictor.register('sds-abstractive-predictor')
+class AbstractivePredictor(Predictor):
+    @overrides
+    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+        return self._dataset_reader.text_to_instance(**json_dict)
+
+    @overrides
+    def dump_line(self, outputs: JsonDict) -> str:
+        summary = outputs['summary']
+        output_data = {'summary': [summary]}
+        return json.dumps(output_data) + '\n'
