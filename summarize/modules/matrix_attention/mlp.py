beacad6fa650d3a233d6050456489af112977f0d
Daniel Deutsch <danfdeutsch@gmail.com>
2019-05-24 19:14:06 -0400

    Adding the MLP matrix attention

---
+++ summarize/modules/matrix_attention/mlp.py
@@ -0,0 +1,60 @@
+import torch
+from allennlp.modules.matrix_attention import MatrixAttention
+from overrides import overrides
+
+
+@MatrixAttention.register('mlp')
+class MLPAttention(MatrixAttention):
+    """
+    An implementation of the "concat" attention from the arvix version of
+    Luong et al. (2015) (https://arxiv.org/pdf/1508.04025.pdf). For some reason,
+    the "concat" attention is different in the version in the ACL Anthology.
+
+    Parameters
+    ----------
+    encoder_size: ``int``
+        The size of the encoder hidden states.
+    decoder_size: ``int``
+        The size of the decoder hidden states.
+    attention_size: ``int``
+        The size of the intermediate attention hidden size.
+    """
+    def __init__(self,
+                 encoder_size: int,
+                 decoder_size: int,
+                 attention_size: int) -> None:
+        super().__init__()
+        self.linear_context = torch.nn.Linear(encoder_size, attention_size, bias=False)
+        self.linear_query = torch.nn.Linear(decoder_size, attention_size, bias=False)
+        self.v = torch.nn.Linear(attention_size, 1, bias=True)
+
+    @overrides
+    def forward(self,
+                decoder_outputs: torch.Tensor,
+                encoder_outputs: torch.Tensor) -> torch.Tensor:
+        """
+        Parameters
+        ----------
+        decoder_outputs: ``torch.Tensor``, ``(batch_size, num_summary_tokens, decoder_size)``
+            The decoder outputs
+        encoder_outputs: ``torch.Tensor``, ``(batch_size, num_document_tokens, encoder_size)``
+
+        Returns
+        -------
+        A ``(batch_size, num_summary_tokens, num_decoder_tokens)``-sized tensor with the
+            unnormalized attention scores.
+        """
+        num_decoder_tokens = decoder_outputs.size(1)
+        num_encoder_tokens = encoder_outputs.size(1)
+
+        decoder_outputs = decoder_outputs.unsqueeze(2)
+        encoder_outputs = encoder_outputs.unsqueeze(1)
+
+        decoder_projection = self.linear_query(decoder_outputs)
+        encoder_projection = self.linear_context(encoder_outputs)
+
+        decoder_projection = decoder_projection.expand(-1, -1, num_encoder_tokens, -1)
+        encoder_projection = encoder_projection.expand(-1, num_decoder_tokens, -1, -1)
+
+        affinities = self.v(torch.tanh(decoder_projection + encoder_projection)).squeeze(-1)
+        return affinities
