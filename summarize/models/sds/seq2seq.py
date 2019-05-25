3328316222c864a9d06600dae93939a8b9299445
Daniel Deutsch <danfdeutsch@gmail.com>
2019-05-24 23:49:26 -0400

    Fixing default variable bug

--- summarize/models/sds/seq2seq.py
+++ summarize/models/sds/seq2seq.py
@@ -95,7 +95,7 @@ class Seq2SeqModel(Model):
         self.decoder = decoder
         self.hidden_projection_layer = hidden_projection_layer
         self.memory_projection_layer = memory_projection_layer
-        self.summary_token_embedder = summary_token_embedder
+        self.summary_token_embedder = summary_token_embedder or document_token_embedder._token_embedders['tokens']
         self.summary_namespace = summary_namespace
         self.use_input_feeding = use_input_feeding
         self.input_feeding_projection_layer = input_feeding_projection_layer
