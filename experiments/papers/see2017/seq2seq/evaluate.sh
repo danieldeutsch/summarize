d2d2e8e358cd11e073a35109bc4ce2ac017e53c0
Daniel Deutsch <danfdeutsch@gmail.com>
2019-06-10 12:18:53 -0400

    Creating output directory if it does not exist

--- experiments/papers/see2017/seq2seq/evaluate.sh
+++ experiments/papers/see2017/seq2seq/evaluate.sh
@@ -2,6 +2,8 @@ expt_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 output_dir=${expt_dir}/output
 results_dir=${expt_dir}/results
 
+mkdir -p ${results_dir}
+
 for split in valid test; do
   python -m summarize.metrics.rouge \
     https://s3.amazonaws.com/danieldeutsch/summarize/data/cnn-dailymail/cnn-dailymail/${split}.tokenized.v1.0.jsonl.gz \
