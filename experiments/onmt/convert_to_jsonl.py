62f17cd5973ce3e42421d9b4c0e25fab5a4a31f0
Daniel Deutsch <danfdeutsch@gmail.com>
2019-07-30 08:10:42 +0200

    Adding tsv to jsonl script

---
+++ experiments/onmt/convert_to_jsonl.py
@@ -0,0 +1,32 @@
+"""
+Converts the output of the OpenNMT models to the jsonl format that
+is necessary for evaluation. Additionally, the script will remove the
+sentence delimiters from the output.
+"""
+# Edit the system path so the summarize library can be imported
+import sys
+sys.path.append('../..')
+
+import argparse
+import json
+
+from summarize.data.io import JsonlWriter
+
+
+def main(args):
+    with JsonlWriter(args.output_jsonl) as out:
+        with open(args.input_tsv, 'r') as f:
+            for line in f:
+                line = line.strip()
+                line = line.replace('<t>', '').replace('</t>', '')
+                line = ' '.join(line.split())
+                summary = [line]
+                out.write(json.dumps({'summary': summary}) + '\n')
+
+
+if __name__ == '__main__':
+    argp = argparse.ArgumentParser()
+    argp.add_argument('input_tsv', help='The output from the OpenNMT model')
+    argp.add_argument('output_jsonl', help='The converted jsonl file')
+    args = argp.parse_args()
+    main(args)
