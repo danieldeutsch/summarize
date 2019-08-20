e0dfffdb144c0bac26a92b1bee5df09967d9ac4b
Daniel Deutsch <danfdeutsch@gmail.com>
2019-08-20 20:39:49 +0200

    Adding preprocess script, updating run

--- experiments/deutsch2019/extractive-step/oracle/run.sh
+++ experiments/deutsch2019/extractive-step/oracle/run.sh
@@ -3,14 +3,7 @@ output_dir="${expt_dir}/output"
 results_dir="${expt_dir}/results"
 mkdir -p ${output_dir} ${results_dir}
 
-date="2019-04-20"
-max_words=200
-
 for split in valid test; do
-  gold_file="data/wikicite/${date}/preprocessed/${split}.jsonl.gz"
-  model_file="${output_dir}/${split}.jsonl"
-  metrics_file="${output_dir}/${split}.metrics.json"
-
   python -m summarize.utils.extract_cloze_from_labels \
     https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz \
     ${output_dir}/${split}.jsonl
@@ -23,5 +16,6 @@ for split in valid test; do
     --add-wrapping-list \
     --compute-rouge-l \
     --silent \
+    --max-words 200 \
     --output-file ${results_dir}/${split}.metrics.json
 done
---
+++ summarize/utils/copy_jsonl_fields.py
@@ -0,0 +1,22 @@
+import argparse
+
+from summarize.data.io import JsonlReader, JsonlWriter
+
+
+def main(args):
+    with JsonlWriter(args.output_jsonl) as out:
+        with JsonlReader(args.source_jsonl) as source:
+            with JsonlReader(args.target_jsonl) as target:
+                for field in args.field_names:
+                    target[field] = source[field]
+                out.write(target)
+
+
+if __name__ == '__main__':
+    argp = argparse.ArgumentParser()
+    argp.add_argument('source_jsonl', help='The file with the desired field')
+    argp.add_argument('target_jsonl', help='The destination file')
+    argp.add_argument('output_jsonl', help='The file with the target data and copied source field')
+    argp.add_argument('field_names', nargs='+', help='The name of the fields to copy')
+    args = argp.parse_args()
+    main(args)
