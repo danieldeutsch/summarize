234285669efa0478cc939170676790da1b812271
Daniel Deutsch <danfdeutsch@gmail.com>
2019-08-20 09:20:40 +0200

    Adding sweep over metrics

--- experiments/deutsch2019/baselines/oracle/run.sh
+++ experiments/deutsch2019/baselines/oracle/run.sh
@@ -4,20 +4,22 @@ results_dir="${expt_dir}/results"
 mkdir -p ${output_dir} ${results_dir}
 
 for split in valid test; do
-  python -m summarize.models.cloze.oracle \
-    https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz \
-    ${output_dir}/${split}.jsonl \
-    R1-F1 \
-    --max-sentences 1 \
-    --cloze-only
+  for metric in "R1-F1" "R2-F1" "RL-F1"; do
+    python -m summarize.models.cloze.oracle \
+      https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz \
+      ${output_dir}/${split}.${metric}.jsonl \
+      ${metric} \
+      --max-sentences 1 \
+      --cloze-only
 
-  python -m summarize.metrics.rouge \
-    https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz \
-    ${output_dir}/${split}.jsonl \
-    --gold-summary-field-name cloze \
-    --model-summary-field-name cloze \
-    --add-wrapping-list \
-    --compute-rouge-l \
-    --silent \
-    --output-file ${results_dir}/${split}.metrics.json
+    python -m summarize.metrics.rouge \
+      https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz \
+      ${output_dir}/${split}.${metric}.jsonl \
+      --gold-summary-field-name cloze \
+      --model-summary-field-name cloze \
+      --add-wrapping-list \
+      --compute-rouge-l \
+      --silent \
+      --output-file ${results_dir}/${split}.${metric}.metrics.json
+  done
 done
