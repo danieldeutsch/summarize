2c26b2172dbc33930a4f2df1fe6a69e43dc484a0
Daniel Deutsch <danfdeutsch@gmail.com>
2019-08-21 08:36:07 +0200

    Adding evaluate and predict

---
+++ experiments/deutsch2019/extractive-step/extractive-model/evaluate.sh
@@ -0,0 +1,45 @@
+if [ "$#" -ne 2 ]; then
+    echo "Usage: sh evaluate.sh <use-topics> <use-context>"
+    exit
+fi
+
+use_topics=$1
+use_context=$2
+if [ "${use_topics}" == "true" ]; then
+  topics_dir="topics"
+else
+  topics_dir="no-topics"
+fi
+if [ "${use_context}" == "true" ]; then
+  context_dir="context"
+else
+  context_dir="no-context"
+fi
+
+expt_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+output_dir=${expt_dir}/output/${topics_dir}/${context_dir}
+results_dir=${expt_dir}/output/${topics_dir}/${context_dir}
+mkdir -p ${results_dir}
+
+for split in valid test; do
+  python -m summarize.metrics.rouge \
+    https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz \
+    ${output_dir}/${split}.max-tokens.jsonl \
+    --gold-summary-field-name cloze \
+    --model-summary-field-name cloze \
+    --add-gold-wrapping-list \
+    --compute-rouge-l \
+    --silent \
+    --max-words 200 \
+    --output-file ${results_dir}/${split}.max-tokens.metrics.json
+
+  python -m summarize.metrics.rouge \
+    https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz \
+    ${output_dir}/${split}.max-sents.jsonl \
+    --gold-summary-field-name cloze \
+    --model-summary-field-name cloze \
+    --add-gold-wrapping-list \
+    --compute-rouge-l \
+    --silent \
+    --output-file ${results_dir}/${split}.max-sents.metrics.json
+done
---
+++ experiments/deutsch2019/extractive-step/extractive-model/predict.sh
@@ -0,0 +1,48 @@
+if [ "$#" -ne 2 ]; then
+    echo "Usage: sh predict.sh <use-topics> <use-context>"
+    exit
+fi
+
+use_topics=$1
+use_context=$2
+if [ "${use_topics}" == "true" ]; then
+  topics_dir="topics"
+else
+  topics_dir="no-topics"
+fi
+if [ "${use_context}" == "true" ]; then
+  context_dir="context"
+else
+  context_dir="no-context"
+fi
+
+expt_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+model_file=${expt_dir}/model/${topics_dir}/${context_dir}/model.tar.gz
+output_dir=${expt_dir}/output/${topics_dir}/${context_dir}
+mkdir -p ${output_dir}
+
+for split in valid test; do
+  allennlp predict \
+    --include-package summarize \
+    --predictor cloze-extractive-predictor \
+    --output-file ${output_dir}/${split}.max-tokens.jsonl \
+    --cuda-device 0 \
+    --batch-size 1 \
+    --silent \
+    --use-dataset-reader \
+    --overrides '{"dataset_reader.max_num_sentences": null}' \
+    ${model_file} \
+    https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz
+
+  allennlp predict \
+    --include-package summarize \
+    --predictor cloze-extractive-predictor \
+    --output-file ${output_dir}/${split}.max-sents.jsonl \
+    --cuda-device 0 \
+    --batch-size 1 \
+    --silent \
+    --use-dataset-reader \
+    --overrides '{"dataset_reader.max_num_sentences": null, "model.max_words": null, "model.max_sents": 1}' \
+    ${model_file} \
+    https://danieldeutsch.s3.amazonaws.com/summarize/data/deutsch2019/${split}.v1.0.jsonl.gz
+done
