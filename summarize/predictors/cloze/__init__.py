019d5e1631663ad5f0f416006f9ca1a147170506
Daniel Deutsch <danfdeutsch@gmail.com>
2019-08-25 09:01:28 +0200

    Typo

--- summarize/predictors/cloze/__init__.py
+++ summarize/predictors/cloze/__init__.py
@@ -1,2 +1,2 @@
-from summarize.predictors.cloze.abstractive ClozeAbstractivePredictor
+from summarize.predictors.cloze.abstractive import ClozeAbstractivePredictor
 from summarize.predictors.cloze.extractive import ClozeExtractivePredictor
