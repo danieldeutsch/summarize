d5118516014c0ab136da5520b5dfe0ccda334ad2
Daniel Deutsch <danfdeutsch@gmail.com>
2019-08-28 00:08:40 +0200

    Updating hyperparameters, adding scripts

---
+++ experiments/deutsch2019/abstractive-step/coverage/evaluate.sh
@@ -0,0 +1,35 @@
+expt_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: sh evaluate.sh <preprocessing-dataset> <use-context>"
+    exit
+fi
+
+preprocessing_dataset=$1
+use_context=$2
+if [ "${preprocessing_dataset}" == "lead" ]; then
+  preprocess_dir="${expt_dir}/../../extractive-step/lead/preprocessed"
+fi
+
+if [ "${use_context}" == "true" ]; then
+  context_dir="context"
+else
+  context_dir="no-context"
+fi
+
+output_dir=${expt_dir}/output/${preprocessing_dataset}/${context_dir}
+results_dir=${expt_dir}/results/${preprocessing_dataset}/${context_dir}
+mkdir -p ${results_dir}
+
+for split in valid test; do
+  python -m summarize.metrics.rouge \
+    ${preprocess_dir}/${split}.jsonl.gz \
+    ${output_dir}/${split}.jsonl \
+    --gold-summary-field-name cloze \
+    --model-summary-field-name cloze \
+    --add-gold-wrapping-list \
+    --add-model-wrapping-list \
+    --compute-rouge-l \
+    --silent \
+    --output-file ${results_dir}/${split}.metrics.json
+done
---
+++ experiments/deutsch2019/abstractive-step/coverage/predict.sh
@@ -0,0 +1,36 @@
+expt_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: sh predict.sh <preprocessing-dataset> <use-context>"
+    exit
+fi
+
+preprocessing_dataset=$1
+use_context=$2
+if [ "${preprocessing_dataset}" == "lead" ]; then
+  preprocess_dir="${expt_dir}/../../extractive-step/lead/preprocessed"
+fi
+
+if [ "${use_context}" == "true" ]; then
+  context_dir="context"
+else
+  context_dir="no-context"
+fi
+
+model_dir=${expt_dir}/model/${preprocessing_dataset}/${context_dir}
+model_file=${model_dir}/model.tar.gz
+output_dir=${expt_dir}/output/${preprocessing_dataset}/${context_dir}
+mkdir -p ${output_dir}
+
+for split in valid test; do
+  allennlp predict \
+    --include-package summarize \
+    --output-file ${output_dir}/${split}.jsonl \
+    --predictor cloze-abstractive-predictor \
+    --silent \
+    --use-dataset-reader \
+    --cuda-device 0 \
+    --batch-size 16 \
+    ${model_file} \
+    ${preprocess_dir}/${split}.jsonl.gz
+done
