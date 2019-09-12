ef7a915e34df18b6c44658139867701ca942b500
Daniel Deutsch <danfdeutsch@gmail.com>
2019-09-12 15:06:07 -0400

    Removing no constraints

--- experiments/onmt/seq2seq/predict.sh
+++ experiments/onmt/seq2seq/predict.sh
@@ -5,19 +5,6 @@ output_dir=${expt_dir}/output
 mkdir -p ${output_dir}
 
 for split in valid test; do
-  # No extra constraints
-  allennlp predict \
-    --include-package summarize \
-    --predictor sds-abstractive-predictor \
-    --output-file ${output_dir}/${split}.none.jsonl \
-    --cuda-device 0 \
-    --batch-size 16 \
-    --silent \
-    --use-dataset-reader \
-    --overrides '{"model.beam_search.min_steps": null, "model.beam_search.disallow_repeated_ngrams": null, "model.beam_search.repeated_ngrams_exceptions": null, "model.beam_search.length_penalizer": null, "model.beam_search.coverage_penalizer": null}' \
-    ${model_file} \
-    https://s3.amazonaws.com/danieldeutsch/summarize/data/cnn-dailymail/cnn-dailymail/${split}.tokenized.v1.0.jsonl.gz
-
   # add minimum length
   allennlp predict \
     --include-package summarize \
