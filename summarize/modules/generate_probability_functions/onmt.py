dbd393a1494093f711f9e668d67385532997e5ae
Daniel Deutsch <danfdeutsch@gmail.com>
2019-10-17 10:28:00 -0400

    Removing stability hack

--- summarize/modules/generate_probability_functions/onmt.py
+++ summarize/modules/generate_probability_functions/onmt.py
@@ -27,15 +27,4 @@ class ONMTGenerateProbabilityFunction(GenerateProbabilityFunction):
                 post_attention_decoder_outputs: torch.Tensor,
                 attention_context: torch.Tensor) -> torch.Tensor:
         # shape: (batch_size, num_summary_tokens)
-        probability = torch.sigmoid(self.hidden_layer(post_attention_decoder_outputs).squeeze(2))
-
-        # In my experience, the generation probability can sometimes be equal
-        # to 1.0 or 0.0 (with really large/small scores) even with reasonably sized
-        # parameter values. This causes problems with the log which is called
-        # later on. Therefore, we move the probability closer to 0.5 by a small
-        # number for stability.
-        # shape: (batch_size, num_summary_tokens)
-        geq_one_half_mask = (probability >= 0.5).float()
-        # shape: (batch_size, num_summary_tokens)
-        probability = (probability - 1e-3) * (geq_one_half_mask) + (probability + 1e-3) * (1 - geq_one_half_mask)
-        return probability
+        return torch.sigmoid(self.hidden_layer(post_attention_decoder_outputs).squeeze(2))
