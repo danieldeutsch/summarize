49ad8139b22edb28ddb88edf0df00eb6718a6441
Daniel Deutsch <danfdeutsch@gmail.com>
2019-10-19 14:14:21 -0400

    Adding demo

--- summarize/predictors/cloze/abstractive.py
+++ summarize/predictors/cloze/abstractive.py
@@ -1,11 +1,21 @@
 import json
 from allennlp.common.util import JsonDict
+from allennlp.data import Instance
 from allennlp.service.predictors.predictor import Predictor
 from overrides import overrides
 
 
 @Predictor.register('cloze-abstractive-predictor')
 class ClozeAbstractivePredictor(Predictor):
+    @overrides
+    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+        document = json_dict['document']
+        topics = json_dict['topics']
+        context = json_dict['context']
+        return self._dataset_reader.text_to_instance(document=document,
+                                                     topics=topics,
+                                                     context=context)
+
     @overrides
     def dump_line(self, outputs: JsonDict) -> str:
         cloze = outputs['cloze']
